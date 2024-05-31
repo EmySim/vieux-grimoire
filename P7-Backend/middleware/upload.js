6593bf33d113077160d42a33a117f4ee90b60940
EmySim <emiliesimson@gmail.com>
2024-06-01 00:28:04 +0200

    mémoire virtuelle multer

--- P7-Backend/middleware/upload.js
+++ P7-Backend/middleware/upload.js
@@ -1,20 +1,16 @@
-const multer = require('multer');
-const sharp = require('sharp');
-const path = require('path');
-const fs = require('fs');
+const multer = require("multer");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
-// Définir les types la nature et le format 
+/*// Définir les types la nature et le format 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
-//configuration du stockage Multer
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
+
 
   //nom de fichier
   filename: (req, file, callback) => {
@@ -25,7 +21,11 @@ const storage = multer.diskStorage({
 });
 
 // Middleware de téléchargement avec Multer
-const upload = multer({ storage: storage }).single('image');
+const upload = multer({ storage: storage }).single('image');*/
+
+//configuration du stockage Multer dans la mémoire virtuelle
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage }).single("image");
 
 // Redimensionnement de l'image et conversion en WebP
 const resizeImage = (req, res, next) => {
@@ -33,31 +33,25 @@ const resizeImage = (req, res, next) => {
     return next();
   }
 
-  const filePath = req.file.path;
-  const fileName = req.file.filename.split('.')[0]; 
-  const outputFilePath = path.join('images', `${fileName}.webp`);
+  const filePath = req.file.buffer;
+  const fileName = req.file.originalname.split(".")[0];
+  const outputFilePath = path.join("images", `${fileName}.webp`);
+  console.log(outputFilePath);
 
   sharp(filePath)
     .resize({ width: 206, height: 260 })
-    .toFormat('webp')
+    .toFormat("webp")
     .toFile(outputFilePath)
     .then(() => {
       //remplace par le fichier redimensionné
-      fs.unlink(filePath, (err) => {
-        if (err) {
-          console.error(err);
-          return next(err);
-        }
-        req.file.path = outputFilePath;
-        req.file.filename = `${fileName}.webp`;
-        next();
-      });
+      req.file.path = outputFilePath;
+      req.file.filename = `${fileName}.webp`;
+      next();
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       return next(err);
     });
 };
 
 module.exports = { upload, resizeImage };
-
